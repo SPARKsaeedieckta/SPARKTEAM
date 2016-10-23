9ab19346a47e00064dc29f2d182b7babbc6183f8
SPARKsaeedieckta <saeedasadnezhad1@gmail.com>
2016-10-23 05:37:20 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,20 +226,20 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {273810826,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
+https://githup.com/SPARKsaeedieckta/SPARKTEAM
 
 Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
+Alphonse (@saeed_ieckta_bot)
+I M /-\ N (@saeed_ieckta_bot)
+Siyanew (@saeed_ieckta_bot)
+Rondoozle (@saeed_ieckta_bot)
+Seyedan (@Saeed_ieckta_bot)
 
 Special thanks to:
 Juan Potato
@@ -248,8 +248,8 @@ Topkecleon
 Vamptacus
 
 Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
+English: @TEAM_SPARK
+Persian: @TEAM_spark
 ]],
     help_text_realm = [[
 Realm Commands:
